4d2bfd69fc535808c172e32f4fde13372882f36a
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2012-12-20 10:12:08 -0200

    Study rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user]) 
+      redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Colchonet::Application.routes.draw do
+
   resources :rooms
   resources :users
+
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +54,7 @@ Colchonet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
